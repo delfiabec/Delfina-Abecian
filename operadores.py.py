fd292dd51d32db14ef77c8567d2220cb9c78739f
Delfina Abecian <delfiabecian@gmail.com>
2026-06-18 18:34:28 -0300

    Add files via upload

---
+++ operadores.py.py
@@ -0,0 +1,37 @@
+#operadores
+print(3 + 4)
+print(10 % 2)#para ver si es divisible
+
+print(10 // 3) #aproxima a diviosn entera
+print(2 ** 3)
+
+print("hola" + "python")
+print("hola" + str(5))# pongo str para que sea string
+
+print("hola " * 5) #me da hola 5 veces
+print(3 < 4)
+print(3 > 4) #false
+print(3 == 4) #false
+print(3 != 4) #false
+print(3 > 4 == 2)#false
+print("hola" > "python") #false
+
+print("hola" >+ "zola") #false proque no es igual
+print(len("hola") >+ len("zola")) #true 
+
+
+print(3 > 4 and "hola" > "python") #false
+print(3 > 4 or "hola" > "python") #false
+print(3 < 4 or "hola" < "python") #true
+print(3 < 4 and "hola" < "python") #true
+print(3 > 4 and "hola" < "python") #false
+print(3 > 4 or "hola" < "python") #true
+
+
+print(3 > 4)#false 
+print(not(3 > 4))#true 
+
+
+
+
+
